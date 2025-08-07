0b698b89f6784bbec088a8a9daf3d84873677f4a
Shuu <61757068+ShuoTheOne@users.noreply.github.com>
2025-08-07 11:10:17 +0200

    0.2
    Modified config files for proxy and docker

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
+
+
 <script setup></script>
+
+
 
 <template>
   <h1>You did it!</h1>
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    server: {
+      host: env.VITE_HOST || 'localhost',
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: 'http://backend:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api'),
+        },
+      },
     },
-  },
+  }
 })
